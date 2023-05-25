6679cae494bf49b2b45abc2e79507b4e9c9f52c7
Priyanka Kandalkar <124030111+kandalkar123@users.noreply.github.com>
2023-05-25 14:07:23 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,16 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include<conio.h>
 using namespace std;
 
 int main()
 {
     int n, a[n];
     int First=0,Last=n-1 , Mid, Num;
-
+    
     cout <<"Enter no of elements : ";
     cin>>n;
-
+    
     cout <<"Enter elements : ";
-
+    
     for(int i=0; i<n; i++){
         cout<<i+1<<") ";
         cin>>a[i];    
@@ -18,7 +19,7 @@ int main()
 
     cout<<" Enter the number to find : ";
     cin >>Num;
-
+    
     do
     {
         Mid=(First+Last)/2;
@@ -30,13 +31,13 @@ int main()
             First = Mid+1;
         else
             Last=Mid-1;
-
-
+         
+        
     }
     while (First<=Last);
-
+    
     if(First>Last)
         cout<<"element not found";
-
+        
     return 0;
 }
